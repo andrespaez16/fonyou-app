51a4e2ca2f264d826186807549cfa44593a068e3
andrespaez216 <kinkbmx216@gmail.com>
2019-06-27 20:32:19 -0500

    fixed responsive and create docs and fixed template

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,8 +1,11 @@
-const config ={
-    // url:'http://localhost:8080/employee',
-    url:'http://51.77.136.28:3000/employee',
-    endpoints:[
-    'employee','allemployees'
+const config = {
+    url: 'http://localhost:8080/employee',
+
+    //Este es unAPIREST creado para hacer pruebas locales si 
+    //desean probarla esta activa
+    // url:'http://51.77.136.28:3000/employee',
+    endpoints: [
+        'employee', 'allemployees'
     ]
 }
 export {
